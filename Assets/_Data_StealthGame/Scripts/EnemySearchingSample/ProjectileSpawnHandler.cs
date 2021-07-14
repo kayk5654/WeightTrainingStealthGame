0d02a2573b484c1f01518fecc039071ece333fc0
kayk5654 <kayk5654@gmail.com>
2021-07-14 21:19:18 +0900

    add a system to test object spawn

--- Assets/_Data_StealthGame/Scripts/EnemySearchingSample/ProjectileSpawnHandler.cs
+++ Assets/_Data_StealthGame/Scripts/EnemySearchingSample/ProjectileSpawnHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 /// <summary>
 /// spawn projectile to search object 
 /// for testing purpose
@@ -13,17 +14,23 @@ public class ProjectileSpawnHandler : MonoBehaviour
     [SerializeField, Tooltip("guide transform to spawn projectiles (optional)")]
     private Transform _spawnGuide;
 
+    [SerializeField, Tooltip("manage in-game input")]
+    private InGameInputManager _inGameInputManager;
 
+
+    /// <summary>
+    /// initialization
+    /// </summary>
     private void Start()
     {
-        
+        _inGameInputManager._onPush += Spawn;
     }
 
     /// <summary>
     /// spawn projectile by any action
     /// it'll be called as a callback of input
     /// </summary>
-    private void Spawn()
+    private void Spawn(object sender, EventArgs e)
     {
         ProjectileBase projectile = Instantiate(_projectile, _spawnGuide.position, _spawnGuide.rotation, null);
         projectile.SetMoveDirection(_spawnGuide.forward);
---
+++ Assets/_Data_StealthGame/Scripts/InGameInput/InGameInputManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+/// <summary>
+/// manages various input to control in-game objects;
+/// keyboard, air-tap, exercise movement, etc.
+/// for testing purpose
+/// </summary>
+public class InGameInputManager : MonoBehaviour
+{
+    // types of input
+    [System.Serializable]
+    private enum InputType
+    {
+        keyboard,
+        airTap,
+        exercise,
+    }
+
+    [SerializeField, Tooltip("type of input to control in-game objects")]
+    private InputType _inputType;
+
+    // actual input to control in-game objects
+    //private IInGameInput _inGameInput;
+
+    // event called when the movement to push player's body up is detected
+    public event EventHandler _onPush;
+
+    // event called when the player starts keeping the lowest posture
+    public event EventHandler _onStartHold;
+
+    // event called when the player stops keeping the lowest posture
+    public event EventHandler _onStoptHold;
+
+    /// <summary>
+    /// initialization of input type
+    /// </summary>
+    private void Start()
+    {
+        switch (_inputType)
+        {
+            case InputType.keyboard:
+                break;
+
+            case InputType.airTap:
+                break;
+
+            case InputType.exercise:
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+    private void Update()
+    {
+        switch (_inputType)
+        {
+            case InputType.keyboard:
+                if (Input.GetKeyDown(KeyCode.Alpha1))
+                {
+                    _onPush(this, EventArgs.Empty);
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha2))
+                {
+                    _onStartHold(this, EventArgs.Empty);
+                }
+
+                if (Input.GetKeyUp(KeyCode.Alpha2))
+                {
+                    _onStoptHold(this, EventArgs.Empty);
+                }
+                break;
+
+            case InputType.airTap:
+                break;
+
+            case InputType.exercise:
+                break;
+
+            default:
+                break;
+        }
+    }
+}
