f192050e5766e66a31ec57f2cdb3439bdbcdd42e
kayk5654 <kayk5654@gmail.com>
2021-09-26 20:58:36 +0900

    enalbe to connect nodes together

---
+++ Assets/_Data_StealthGame/Scripts/ObjectControl/Node.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+/// <summary>
+/// contain parameters of node
+/// </summary>
+public class Node : MonoBehaviour
+{
+    public NodesManager _nodesManager;
+
+    public float _range = 1f;
+
+    private bool _isConencted;
+
+    private LineRenderer[] _lines;
+
+    public float _speed = 1f;
+
+    private float _lineWidth = 0.006f;
+
+    public Material _lineMaterial;
+
+    [SerializeField, Tooltip("node mesh")]
+    private MeshRenderer _mainMeshRenderer;
+
+    private Material _nodeMaterial;
+
+    [SerializeField, Tooltip("node cap")]
+    private List<Transform> _nodeCaps = new List<Transform>();
+
+    WaitForEndOfFrame _waitForEndOfFrame;
+
+    /// <summary>
+    /// initialization
+    /// </summary>
+    private void Start()
+    {
+        _nodeMaterial = _mainMeshRenderer.material;
+        WaitForEndOfFrame _waitForEndOfFrame = new WaitForEndOfFrame();
+        _nodeCaps[0].gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// connect this node to another node
+    /// </summary>
+    public void Connect()
+    {
+        _isConencted = true;
+        _mainMeshRenderer.enabled = true;
+        StartCoroutine(ConnectSequence());
+    }
+
+    /// <summary>
+    /// connection process to other nodes
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ConnectSequence()
+    {
+        // search connectable nodes
+        Node[] connectableNodes = _nodesManager.GetConnectableNodes(transform.position, _range);
+
+        float phase = 0f;
+        _lines = new LineRenderer[connectableNodes.Length];
+        for (int i = 0; i < _lines.Length; i++)
+        {
+            // initialize connection lines
+            LineRenderer lineObject = new GameObject().AddComponent<LineRenderer>();
+            lineObject.transform.SetParent(transform);
+            lineObject.transform.localPosition = Vector3.zero;
+            lineObject.transform.localRotation = Quaternion.identity;
+            _lines[i] = lineObject;
+            _lines[i].useWorldSpace = false;
+            _lines[i].SetPosition(0, Vector3.zero);
+            _lines[i].SetPosition(1, Vector3.zero);
+            _lines[i].material = _lineMaterial;
+            _lines[i].startWidth = _lineWidth;
+            _lines[i].endWidth = _lineWidth;
+            _lines[i].shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+            // duplicate node caps for each connections
+            if (i > 0)
+            {
+                Transform newNode = Instantiate(_nodeCaps[0].gameObject, _nodeCaps[0].parent).transform;
+                newNode.LookAt(connectableNodes[i].transform);
+                _nodeCaps.Add(newNode);
+                newNode.gameObject.SetActive(true);
+            }
+            else
+            {
+                _nodeCaps[0].transform.LookAt(connectableNodes[0].transform);
+                _nodeCaps[0].gameObject.SetActive(true);
+            }
+            
+        }
+
+        // connect nodes
+        IEnumerator[] connectionSequences = new IEnumerator[connectableNodes.Length];
+
+        for(int i = 0; i < connectableNodes.Length; i++)
+        {
+            connectionSequences[i] = SingleConnectionSequence(_lines[i], connectableNodes[i], _speed * Random.Range(0.5f, 1.5f));
+            StartCoroutine(connectionSequences[i]);
+        }
+
+        yield return null;
+    }
+
+    private IEnumerator SingleConnectionSequence(LineRenderer line, Node node, float speed)
+    {
+        node.SpawnNodeCapForReceiving(transform);
+        
+        float phase = 0f;
+
+        while (phase < 1.0f)
+        {
+            phase += Time.deltaTime * speed;
+
+            line.SetPosition(1, Vector3.Lerp(Vector3.zero, transform.InverseTransformPoint(node.transform.position) * transform.localScale.x, phase));
+            yield return _waitForEndOfFrame;
+        }
+
+        line.SetPosition(1, transform.InverseTransformPoint(node.transform.position) * transform.localScale.x);
+        node.Connect();
+    }
+
+    public bool GetIsConnected()
+    {
+        return _isConencted;
+    }
+
+    /// <summary>
+    /// spawn node cap of this node from another node
+    /// </summary>
+    public void SpawnNodeCapForReceiving(Transform lookAtTarget)
+    {
+        Transform newNodeCap = Instantiate(_nodeCaps[0].gameObject, _nodeCaps[0].parent).transform;
+        newNodeCap.LookAt(lookAtTarget);
+        newNodeCap.gameObject.SetActive(true);
+        _nodeCaps.Add(newNodeCap);
+    }
+}
---
+++ Assets/_Data_StealthGame/Scripts/ObjectControl/NodesManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// manages connected nodes
+/// </summary>
+public class NodesManager : MonoBehaviour
+{
+    [SerializeField, Tooltip("nodes without light")]
+    private GameObject _nodePrefab;
+
+    [SerializeField, Tooltip("area to spawn nodes")]
+    private BoxCollider _spawnArea;
+
+    [SerializeField, Tooltip("number of nodes")]
+    private int _nodeCount = 100;
+
+    // array of _nodeWithoutLight instances
+    private Node[] _nodes;
+
+    [SerializeField, Tooltip("material of line")]
+    private Material _lineMaterial;
+
+    [SerializeField, Tooltip("connecting speed")]
+    private float _speed = 1f;
+
+    [SerializeField, Tooltip("connecting range")]
+    private float _range = 1f;
+
+
+    /// <summary>
+    /// initialization
+    /// </summary>
+    private void Start()
+    {
+        SpawnNodes();
+        StartConnect();
+    }
+
+    /// <summary>
+    /// delete nodes without light
+    /// </summary>
+    private void OnDisable()
+    {
+        for(int i = 0; i < _nodes.Length; i++)
+        {
+            Destroy(_nodes[i].gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// generate nodes
+    /// </summary>
+    private void SpawnNodes()
+    {
+        _nodes = new Node[_nodeCount];
+        Vector3 positionTemp = Vector3.zero;
+        for (int i = 0; i < _nodeCount; i++)
+        {
+            Transform newNode = Instantiate(_nodePrefab).transform;
+            positionTemp.x = Random.Range(_spawnArea.bounds.min.x, _spawnArea.bounds.max.x);
+            positionTemp.y = Random.Range(_spawnArea.bounds.min.y, _spawnArea.bounds.max.y);
+            positionTemp.z = Random.Range(_spawnArea.bounds.min.z, _spawnArea.bounds.max.z);
+            newNode.position = positionTemp;
+            newNode.SetParent(this.transform);
+            _nodes[i] = newNode.GetComponent<Node>();
+            _nodes[i]._nodesManager = this;
+            _nodes[i]._lineMaterial = _lineMaterial;
+            _nodes[i]._speed = _speed * Random.Range(0.5f, 1.2f);
+        }
+    }
+
+    /// <summary>
+    /// start connect from the first node
+    /// </summary>
+    private void StartConnect()
+    {
+        _nodes[0]._lineMaterial = _lineMaterial;
+        _nodes[0].Connect();
+    }
+
+    public Node[] GetConnectableNodes(Vector3 origin, float range)
+    {
+        List<Node> connectableNodes = new List<Node>();
+
+        for(int i = 0; i < _nodes.Length; i++)
+        {
+            if (_nodes[i].GetIsConnected()) { continue; }
+            if(Vector3.Distance(origin, _nodes[i].transform.position) > range) { continue; }
+
+            connectableNodes.Add(_nodes[i]);
+        }
+
+        return connectableNodes.ToArray();
+    }
+}
