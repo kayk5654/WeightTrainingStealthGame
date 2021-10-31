266c94671e132fb07a9b5804f97b0769fdd787ad
kayk5654 <kayk5654@gmail.com>
2021-10-31 12:09:05 +0900

    add app starter script

---
+++ Assets/_Data_StealthGame/Scripts/Application/AppStarter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// initialize settings and classes on starting this app
+/// </summary>
+public class AppStarter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
