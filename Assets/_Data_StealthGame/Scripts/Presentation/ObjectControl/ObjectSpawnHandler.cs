a40562cf3629c414847704cfe7c8e9fb3cab86c0
kayk5654 <kayk5654@gmail.com>
2021-11-07 21:29:05 +0900

    add classes to handle object spawn area

---
+++ Assets/_Data_StealthGame/Scripts/Presentation/ObjectControl/ObjectSpawnHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// provide a feature to spawn scene object
+/// </summary>
+public class ObjectSpawnHandler : MonoBehaviour
+{
+    
+    
+    /// <summary>
+    /// create instance of a prefab in the argument
+    /// </summary>
+    /// <param name="spawnObject"></param>
+    /// <returns></returns>
+    public GameObject Spawn(GameObject spawnObject)
+    {
+        GameObject newObject = Instantiate(spawnObject);
+
+        // set transform of newObject, so that it is placed in the spawn area
+
+        return newObject;
+    }
+}
---
+++ Assets/_Data_StealthGame/Scripts/Presentation/ObjectControl/SpawnAreaHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// manage spawn area
+/// enable to get spawn area depending on the exercise type selection
+/// </summary>
+public class SpawnAreaHandler : MonoBehaviour
+{
+    // center of the spawn area
+    private Vector3 _center;
+
+    // size of the spawn area
+    private Vector3 _size;
+
+    // world-space rotation of the spawn area
+    private Quaternion _rotation;
+
+    private Vector3 _boundLocalMin;
+    private Vector3 _boundLocalMax;
+
+    /// <summary>
+    /// overrde spawn area
+    /// </summary>
+    public void GetSpawnArea(out Vector3 center, out Vector3 size, out Quaternion rotation)
+    {
+        center = _center;
+        size = _size;
+        rotation = _rotation;
+    }
+}
