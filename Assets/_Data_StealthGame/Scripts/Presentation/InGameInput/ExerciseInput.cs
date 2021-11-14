9abfc90d417d4c5c1fc41e65dcf2861c5ef729e0
kayk5654 <kayk5654@gmail.com>
2021-11-14 21:58:58 +0900

    add a class to get exercise input

---
+++ Assets/_Data_StealthGame/Scripts/Presentation/InGameInput/ExerciseInput.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+/// <summary>
+/// input from player's movement while exercises
+/// </summary>
+public class ExerciseInput : MonoBehaviour, IInGameInputBase, IActionActivator
+{
+    // event called when the movement to push player's body up is detected
+    public event EventHandler _onPush;
+
+    // event called when the player starts keeping the lowest posture
+    public event EventHandler _onStartHold;
+
+    // event called when the player stops keeping the lowest posture
+    public event EventHandler _onStopHold;
+
+    // whether the input is enabled;
+    private bool _isEnabled;
+
+
+    /// <summary>
+    /// initialization
+    /// </summary>
+    private void Start()
+    {
+        InGameInputSwitcher inGameInputSwitcher = FindObjectOfType<InGameInputSwitcher>();
+        if (!inGameInputSwitcher) { return; }
+        inGameInputSwitcher.SetInputActivator(InputType.exercise, this);
+        inGameInputSwitcher.SetInput(InputType.exercise, this);
+    }
+
+    /// <summary>
+    /// detect input
+    /// </summary>
+    private void Update()
+    {
+        if (!_isEnabled) { return; }
+
+    }
+
+    /// <summary>
+    /// initialize action
+    /// </summary>
+    public void InitAction()
+    {
+
+    }
+
+    /// <summary>
+    /// enable action
+    /// </summary>
+    public void StartAction()
+    {
+        _isEnabled = true;
+    }
+
+    /// <summary>
+    /// disable action
+    /// </summary>
+    public void StopAction()
+    {
+        _isEnabled = false;
+    }
+}
