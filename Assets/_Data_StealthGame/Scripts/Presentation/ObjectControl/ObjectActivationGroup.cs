65e294f6b434850e3c1a15541e9997c6e2c9b34c
kayk5654 <kayk5654@gmail.com>
2021-11-28 16:26:58 +0900

    add highlight on the exercise selection buttons

---
+++ Assets/_Data_StealthGame/Scripts/Presentation/ObjectControl/ObjectActivationGroup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectActivationGroup : MonoBehaviour {
+
+    [SerializeField]
+    private List<GameObject> _objects = new List<GameObject>();
+
+    /// <summary>
+    /// reset selection when this gameobject is enableds
+    /// </summary>
+    private void OnEnable()
+    {
+        SetActive(false);
+    }
+
+    public void SetActive(bool state)
+    {
+        _objects.ForEach(obj => obj.SetActive(state));
+    }
+
+    public void SetSelectedObjActive(int id)
+    {
+        for (int i = 0; i < _objects.Count; i++)
+        {
+            if (i == id)
+            {
+                _objects[i].SetActive(true);
+                continue;
+            }
+            _objects[i].SetActive(false);
+        }
+    }
+}
