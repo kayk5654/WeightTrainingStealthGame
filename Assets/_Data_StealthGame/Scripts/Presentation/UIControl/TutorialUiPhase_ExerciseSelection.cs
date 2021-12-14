d970188e4c0d0b647f556a1e74d868c3fb262a43
kayk5654 <kayk5654@gmail.com>
2021-12-14 16:53:02 +0900

    add interactions on the exercise selection phase of the tutorial

---
+++ Assets/_Data_StealthGame/Scripts/Presentation/UIControl/TutorialUiPhase_ExerciseSelection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class TutorialUiPhase_ExerciseSelection : TutorialUiPhase
+{
+    [SerializeField, Tooltip("button to go to the next phase")]
+    private Interactable _nextButton;
+
+    [SerializeField, Tooltip("toggle plate of the squat button")]
+    private GameObject _squatTogglePlate;
+
+
+    /// <summary>
+    /// initialize when this phase is activated
+    /// </summary>
+    private void OnEnable()
+    {
+        InitButtonState();
+    }
+
+    /// <summary>
+    /// initialize button status
+    /// </summary>
+    private void InitButtonState()
+    {
+        _nextButton.IsEnabled = false;
+        _squatTogglePlate.SetActive(false);
+    }
+}
