ba79c7b495ebac854ec13aff2c6a416ceb555837
kayk5654 <kayk5654@gmail.com>
2021-12-19 09:50:39 +0900

    replace tutorial focus guide

---
+++ Assets/_Data_StealthGame/Scripts/Presentation/ObjectControl/LookAtHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// face transform of specific object to the target
+/// </summary>
+public class LookAtHandler : MonoBehaviour
+{
+    [SerializeField, Tooltip("transform to control")]
+    private Transform _transform;
+
+    [SerializeField, Tooltip("target to look at")]
+    private Transform _lookAtTarget;
+
+
+    /// <summary>
+    /// initialization
+    /// </summary>
+    private void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// update transform
+    /// </summary>
+    private void Update()
+    {
+        _transform.LookAt(_lookAtTarget);
+    }
+}
