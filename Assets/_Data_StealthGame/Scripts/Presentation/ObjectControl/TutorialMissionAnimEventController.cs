8c6d61beef26f93409386a4589ec0b3b83683ebd
kayk5654 <kayk5654@gmail.com>
2021-12-20 19:32:07 +0900

    add vfx on the player's mission animation in the tutorial

--- Assets/_Data_StealthGame/Scripts/Presentation/ObjectControl/TutorialMissionAnimEventController.cs
+++ Assets/_Data_StealthGame/Scripts/Presentation/ObjectControl/TutorialMissionAnimEventController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 /// <summary>
 /// manage events called from an animation clip for "Player's mission" phase of the tutorial
 /// </summary>
@@ -18,8 +19,17 @@ public class TutorialMissionAnimEventController : MonoBehaviour
     [SerializeField, Tooltip("node's destroyed sfx")]
     private AudioClip _nodeDestroyedSfx;
 
+    [SerializeField, Tooltip("vfx of sphere nodes")]
+    private VisualEffect[] _nodeVfxHandlers;
+
+    [SerializeField, Tooltip("renderer of sphere nodes")]
+    private Renderer[] _nodeRenderers;
+
+    // property to switch enemy's animation
     private string _enemyAnimProperty = "IsAttack";
 
+    // property to control damage effect of the nodes
+    private string _nodeDamageProperty = "_DamageRange";
 
     /// <summary>
     /// initialization
@@ -43,6 +53,24 @@ public class TutorialMissionAnimEventController : MonoBehaviour
     }
 
     /// <summary>
+    /// reset state of the objects
+    /// </summary>
+    public void ResetState()
+    {
+        _enemyAnimator.SetBool(_enemyAnimProperty, false);
+
+        for (int i = 0; i < _nodeRenderers.Length; i++) 
+        {
+            if (_nodeRenderers[i].material.HasProperty(_nodeDamageProperty))
+            {
+                _nodeRenderers[i].material.SetFloat(_nodeDamageProperty, 0f);
+            }
+            
+            _nodeRenderers[i].enabled = true;
+        }
+    }
+
+    /// <summary>
     /// set enemy's animation "attack"
     /// </summary>
     public void SetEnemyAttackAnimation()
@@ -53,16 +81,24 @@ public class TutorialMissionAnimEventController : MonoBehaviour
     /// <summary>
     /// play _enemyAttackSfx
     /// </summary>
-    public void PlayEnemyAttackSfx()
+    public void PlayEnemyAttackSfx(int targetNodeId)
     {
         _sfxSource.PlayOneShot(_enemyAttackSfx);
+
+        if (_nodeRenderers[targetNodeId].material.HasProperty(_nodeDamageProperty))
+        {
+            _nodeRenderers[targetNodeId].material.SetFloat(_nodeDamageProperty, 1f);
+        }
     }
 
     /// <summary>
-    /// play _nodeDestroyedSfx
+    /// destroy the selected node
     /// </summary>
-    public void PlayNodeDestroyedSfx()
+    /// <param name="nodeIndex"></param>
+    public void DestroyNode(int nodeIndex)
     {
+        _nodeVfxHandlers[nodeIndex].Play();
         _sfxSource.PlayOneShot(_nodeDestroyedSfx);
+        _nodeRenderers[nodeIndex].enabled = false;
     }
 }
