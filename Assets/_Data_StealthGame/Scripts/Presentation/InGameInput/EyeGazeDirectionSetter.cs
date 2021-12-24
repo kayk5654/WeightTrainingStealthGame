8f27c28ae496249a7abf557fbc229f3823c54e33
kayk5654 <kayk5654@gmail.com>
2021-12-24 23:20:35 +0900

    test to apply view range masking (wip)

---
+++ Assets/_Data_StealthGame/Scripts/Presentation/InGameInput/EyeGazeDirectionSetter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// set looking direction to the shaders
+/// </summary>
+public class EyeGazeDirectionSetter : MonoBehaviour
+{
+    [SerializeField, Tooltip("look direction getter")]
+    private MRTKLookDirectionGetter _mrtkLookDirection;
+
+    // shader property name for the eye gaze
+    private string _eyeGazeDirectionProperty = "_EyeGazeDirection";
+
+    /// <summary>
+    /// initialization
+    /// </summary>
+    private void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// update looking direction
+    /// </summary>
+    private void Update()
+    {
+        Shader.SetGlobalVector(_eyeGazeDirectionProperty, _mrtkLookDirection.GetDirection());
+        //Debug.Log("eye direction = " + _mrtkLookDirection.GetDirection());
+    }
+}
