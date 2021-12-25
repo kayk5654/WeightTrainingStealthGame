f1efd58abcef7e0a3b611ec9e2d725544bef96d8
kayk5654 <kayk5654@gmail.com>
2021-12-25 23:37:52 +0900

    enable to define view range by the eye gaze direction

--- Assets/_Data_StealthGame/Scripts/Presentation/InGameInput/EyeGazeDirectionSetter.cs
+++ Assets/_Data_StealthGame/Scripts/Presentation/InGameInput/EyeGazeDirectionSetter.cs
@@ -9,8 +9,14 @@ public class EyeGazeDirectionSetter : MonoBehaviour
     [SerializeField, Tooltip("look direction getter")]
     private MRTKLookDirectionGetter _mrtkLookDirection;
 
+    [SerializeField, Tooltip("get camera forward direction")]
+    private CameraLookDirectionGetter _cameraLookDirectionGetter;
+
     // shader property name for the eye gaze
     private string _eyeGazeDirectionProperty = "_EyeGazeDirection";
+
+    // shadre property name for the rotation matrix based on the eye gaze
+    private string _eyeGazeSpaceMatrixProperty = "_EyeGazeSpaceMatrix";
 
     /// <summary>
     /// initialization
@@ -26,6 +32,7 @@ public class EyeGazeDirectionSetter : MonoBehaviour
     private void Update()
     {
         Shader.SetGlobalVector(_eyeGazeDirectionProperty, _mrtkLookDirection.GetDirection());
+        //Shader.SetGlobalMatrix(_eyeGazeSpaceMatrixProperty, Matrix4x4.TRS(_mrtkLookDirection.GetOrigin(), Quaternion.FromToRotation(_cameraLookDirectionGetter.GetDirection(), _mrtkLookDirection.GetDirection()), Vector3.one));
         //Debug.Log("eye direction = " + _mrtkLookDirection.GetDirection());
     }
 }
