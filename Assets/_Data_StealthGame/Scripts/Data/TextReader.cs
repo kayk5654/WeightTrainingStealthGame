e68557a5d49e37398f6f00879b41ec0c842ac041
kayk5654 <kayk5654@gmail.com>
2022-07-31 21:04:21 +0900

    enable to load text file from "StreamingAssets"path on Android

--- Assets/_Data_StealthGame/Scripts/Data/TextReader.cs
+++ Assets/_Data_StealthGame/Scripts/Data/TextReader.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
 /// <summary>
 /// read text fron external files
 /// </summary>
@@ -10,7 +12,34 @@ public class TextReader
     /// <returns></returns>
     public string ReadData(string path)
     {
-        string text = File.ReadAllText(path);
+        string text = "";
+
+#if UNITY_ANDROID
+        if(path.Contains(Application.streamingAssetsPath))
+        {
+            var loadingRequest = UnityWebRequest.Get(path);
+            loadingRequest.SendWebRequest();
+            while (!loadingRequest.isDone)
+            {
+                if (loadingRequest.isNetworkError || loadingRequest.isHttpError)
+                {
+                    break;
+                }
+            }
+
+            if (!loadingRequest.isNetworkError && !loadingRequest.isHttpError)
+            {
+                text = loadingRequest.downloadHandler.text;
+            }
+        }
+        else
+        {
+            text = File.ReadAllText(path);
+        }
+        
+#else
+        text = File.ReadAllText(path);
+#endif
 
         return text;
     }
